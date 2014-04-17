ad40a8640686c201d5ca4df23353520c8696d9b6
Alex Montas <Alexmontasmusic@gmail.com>
2014-04-17 18:52:42 -0400

    Functionality is up and running!

--- Shopping.js
+++ Shopping.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
+	$(document).ready(function() {
 	//assigning the click event to the buttom
 	$('#addButton').click(function(){
 
 		var listItem = $('#listItem');
-		$('form[name=listForm]').append('<input type="hidden" name="hiddenList[]" value="' + listItem.val() + '" />');
-		$('#shoppingList').append('<br/>' + input.val());
-		input.val('').focus();
-
+		$('#shoppingList ul').append('<li>' + listItem.val() + "   <span class='close'> X </span>   " + '</li>');
+		listItem.val('').focus();
 
 	});
+
+
+	$('#shoppingList').on("click", "li:contains(X)", function () { 
+    $(this).remove();
+});
+
 
 });
